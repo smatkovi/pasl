431b20f4d698b817148d1deaec481b16438c38e1
s m <sebastian.matkovich@gmail.com>
2024-02-24 18:21:17 +0100

    3 values implemented

--- WriteYLTheta.m
+++ WriteYLTheta.m
@@ -1,9 +1,17 @@
 function [] = WriteYLTheta(filename)
-    x = readFileData(filename)
+    x = readFileData(filename);
     U = 0;
     sizeX = size(x);
+    fileID = fopen(YLTheta.txt','w');
     for i = 1:sizeX(1)-1
         U = U + sqrt( (x(i+1, 1) - x(i, 1))^2 + (x(i+1, 2) - x(i, 2))^2);
+
+        X = (x(sizeX(1)-i, 1) + x(sizeX(1)-i-1, 1))/2;
+        Y = (x(sizeX(1)-i, 2) + x(sizeX(1)-i-1, 2))/2;
+
+        Theta = atan((x(sizeX(1)-i-1, 2) - x(sizeX(1)-i, 2))/(x(sizeX(1)-i-1, 1) - x(sizeX(1)-i, 1)));
+
+        fprintf(fileID,'%f %f %f %f\n',X,Y,Theta,L);
     end
     disp(U);
     
