b2eb9432238e2f55b0a41a3d392696ff5466457b
sm <sebstianmatkovich@gmail.com>
2025-08-07 11:38:27 +0200

    changed order to wingfoil order

--- WriteXYLTheta.m
+++ WriteXYLTheta.m
@@ -9,25 +9,20 @@ function [t] = WriteXYLTheta(filename)
     fileID = fopen('XYLTheta.txt','w');
     %disp(sizeX);
 
-    X = (x(1, 1) + x(sizeX(1), 1))/2;
-    Y = (x(1, 2) + x(sizeX(1), 2))/2;
-    Theta = atan2((x(sizeX(1), 2) - x(1, 2)), (x(sizeX(1), 1) - x(1, 1)));
-    L = sqrt((x(sizeX(1), 1) - x(1, 1))^2 + (x(sizeX(1), 2) - x(1, 2))^2);
-    fprintf(fileID,'%.15e %.15e %.15e %.15e\n',X,Y,L,Theta);
-    for i = 0:sizeX(1)-2
+    for i = 0:sizeX(1)-1
         %i = sizeX(1)-k-1;
         %U = U + sqrt( (x(i+1, 1) - x(i, 1))^2 + (x(i+1, 2) - x(i, 2))^2);
-        %if i < sizeX(1)
+        if i < sizeX(1)-1
             X = (x(sizeX(1)-i, 1) + x(sizeX(1)-i-1, 1))/2;
             Y = (x(sizeX(1)-i, 2) + x(sizeX(1)-i-1, 2))/2;
             Theta = atan2((x(sizeX(1)-i-1, 2) - x(sizeX(1)-i, 2)), (x(sizeX(1)-i-1, 1) - x(sizeX(1)-i, 1)));
             L = sqrt((x(sizeX(1)-i-1, 1) - x(sizeX(1)-i, 1))^2 + (x(sizeX(1)-i-1, 2) - x(sizeX(1)-i, 2))^2);
-        %else 
-        %    X = (x(1, 1) + x(sizeX(1), 1))/2;
-        %    Y = (x(1, 2) + x(sizeX(1), 2))/2;
-        %    Theta = atan2((x(sizeX(1), 2) - x(1, 2)), (x(sizeX(1), 1) - x(1, 1)));
-        %    L = sqrt((x(sizeX(1), 1) - x(1, 1))^2 + (x(sizeX(1), 2) - x(1, 2))^2);
-        %end
+        else 
+            X = (x(1, 1) + x(sizeX(1), 1))/2;
+            Y = (x(1, 2) + x(sizeX(1), 2))/2;
+            Theta = atan2((x(sizeX(1), 2) - x(sizeX(1)-i, 2)), (x(sizeX(1), 1) - x(sizeX(1)-i, 1)));
+            L = sqrt((x(sizeX(1), 1) - x(sizeX(1)-i, 1))^2 + (x(sizeX(1), 2) - x(sizeX(1)-i, 2))^2);
+        end
         
         fprintf(fileID,'%.15e %.15e %.15e %.15e\n',X,Y,L,Theta);
     end
